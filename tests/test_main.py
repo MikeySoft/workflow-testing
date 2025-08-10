ede2f3a20b02c9ec98e1e3ed22e0e0005abd17f3
Michael Verbeek <michael.verbeek@digitalsurvivalcompany.com>
2025-08-10 12:23:06 +0200

    Cover alle

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,14 @@
 import pytest
-from app.main import add
+from app.main import add, main
+
 
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
+
+
+def test_main_entrypoint(capsys):
+    main()
+    captured = capsys.readouterr()
+    assert "Result of add(2, 3): 5" in captured.out
